8dfd8ea0822561eb3d4c3644e9808306c247c901
Ryan Kughn <ryan.h.kughn@gmail.com>
2021-04-27 12:38:48 -0600

    Add in parts to transferEther

--- packages/hardhat/contracts/TransferEther.sol
+++ packages/hardhat/contracts/TransferEther.sol
@@ -4,5 +4,23 @@ pragma solidity ^0.8.4;
 contract TransferEther {
     mapping (address => uint256) userBalance;
 
-    
+    function buyIn(address _user) public {
+
+    }
+
+    function withdrawBalance() public payable {
+        uint _amount = userBalance[msg.sender];
+        userBalance[msg.sender] = 0;
+       payable( msg.sender).transfer(_amount);
+    }
+
+    event Received(address, uint);
+
+    receive () external payable {
+        emit Received(msg.sender, msg.value);
+    }
+
+    fallback () exnternal payable {
+        
+    }
 }
