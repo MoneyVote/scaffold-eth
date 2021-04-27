50ecbada989761f0c05a2e5d9abf13908e9e1fa6
Ryan Kughn <ryan.h.kughn@gmail.com>
2021-04-27 16:01:49 -0600

    Add in new method of payout

--- packages/hardhat/contracts/TransferEther.sol
+++ packages/hardhat/contracts/TransferEther.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.8.4;
 import "./SetupVoting.sol";
 
 contract TransferEther is SetupVoting {
-    mapping (address => uint256) userBalance;
+    
+    mapping (address => bool) hasWithdrawn;
+    uint amount;
 
     function buyIn() public payable {
         require(msg.value == super.getVoteValue());
     }
 
     function withdrawBalance() public payable {
-        uint _amount = userBalance[msg.sender];
-        userBalance[msg.sender] = 0;
-       payable( msg.sender).transfer(_amount);
+        require(hasWithdrawn[msg.sender] == false);
+        hasWithdrawn[msg.sender] = true;
+        payable( msg.sender).transfer(amount);
     }
 
-    function calculateWinnings(uint _totalVotes) public view returns(uint){
+    function calculateWinnings(uint _totalVotes) public{
         uint _contractBal = address(this).balance;
-        uint _individualWinnings = _contractBal/_totalVotes;
-
-        return _individualWinnings;
+        amount = _contractBal/_totalVotes;
     }
 
     event Received(address, uint);
